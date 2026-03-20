a5fd819367e25508ba5fd8f79c3f44de9c00b688
Viraj Mavani <66798733+Viraj-Mavani@users.noreply.github.com>
2026-03-20 17:17:23 -0400

    changed photos

--- lib/data.ts
+++ lib/data.ts
@@ -14,17 +14,22 @@ export interface Service {
 	image: string;
 }
 
+// Pre-Wedding | Wedding | Baby Shower | Product | Photography | Editorial | Event
 export const servicesData: Service[] = [
-	{ id: "s0", title: "Pre-Wedding", image: "/images/placeholders/work0.jpg" },
-	{ id: "s1", title: "Wedding", image: "/images/placeholders/AK1.jpg" },
-	{ id: "s2", title: "Baby Shower", image: "/images/placeholders/work2.jpg" },
-	{ id: "s3", title: "Photography", image: "/images/placeholders/AK3.jpg" },
-	{ id: "s4", title: "Wedding", image: "/images/placeholders/work4.jpg" },
-	{ id: "s5", title: "Wedding", image: "/images/placeholders/work5.jpg" },
-	{ id: "s6", title: "Editorial", image: "/images/placeholders/work6.jpg" },
-	{ id: "s7", title: "Wedding", image: "/images/placeholders/work7.jpg" },
-	{ id: "s8", title: "Photography", image: "/images/placeholders/work8.jpg" },
-	{ id: "s9", title: "Editorial", image: "/images/placeholders/work9.jpg" },
+	{ id: "s0", title: "Wedding", image: "/images/work/work0.jpg" },
+	{ id: "s1", title: "Event", image: "/images/work/work1.jpg" },
+	{ id: "s2", title: "Product", image: "/images/work/work2.jpg" },
+	{ id: "s3", title: "Photography", image: "/images/work/work3.jpg" },
+	{ id: "s4", title: "Pre-Wedding", image: "/images/work/work4.jpg" },
+	{ id: "s5", title: "Photography", image: "/images/work/work5.jpg" },
+	{ id: "s6", title: "Event", image: "/images/work/work6.jpg" },
+	{ id: "s7", title: "Event", image: "/images/work/work7.jpg" },
+	{ id: "s8", title: "Event", image: "/images/work/work8.jpg" },
+	{ id: "s9", title: "Photography", image: "/images/work/work9.jpg" },
+	{ id: "s10", title: "Editorial", image: "/images/work/work10.jpg" },
+	{ id: "s11", title: "Pre-Wedding", image: "/images/work/work11.jpg" },
+	{ id: "s12", title: "Photography", image: "/images/work/work12.jpg" },
+	{ id: "s13", title: "Baby Shower", image: "/images/work/work13.jpg" },
 ];
 
 export const portfolioImages: PortfolioImage[] = [
@@ -32,53 +37,55 @@ export const portfolioImages: PortfolioImage[] = [
 		id: "p1",
 		title: "Aaina & Daideep",
 		category: "Wedding",
-		src: "/images/placeholders/AK4.jpg",
+		src: "/images/feature/feature0.jpg",
 		span: "row-span-2 col-span-1",
 	},
 	{
 		id: "p2",
 		title: "Sapan & Sajnee",
 		category: "Pre-Wedding",
-		src: "/images/placeholders/work7.jpg",
+		src: "/images/feature/feature1.jpg",
 		span: "col-span-1 row-span-1",
 	},
 	{
 		id: "p3",
 		title: "Aditya Gadhvi",
 		category: "Events",
-		src: "/images/placeholders/AK5.jpg",
+		src: "/images/feature/feature2.jpg",
 		span: "col-span-1 row-span-1",
 	},
 	{
 		id: "p4",
 		title: "Vidhi & Kashyap",
 		category: "Baby Shower",
-		src: "/images/placeholders/work2.jpg",
+		src: "/images/feature/feature3.jpg",
 		span: "col-span-2 row-span-2",
 	},
 	{
 		id: "p5",
-		title: "Priya & Preean",
+		title: "MV",
 		category: "Product",
-		src: "/images/placeholders/work5.jpg",
+		src: "/images/feature/feature4.jpg",
 		span: "col-span-1 row-span-1",
 	},
 	{
 		id: "p6",
 		title: "Vidhi & Rushi",
 		category: "Editorial",
-		src: "/images/placeholders/work6.jpg",
+		src: "/images/feature/feature5.jpg",
 		span: "col-span-1 row-span-1",
 	},
 ];
 
 export const backgroundImages = [
-	"/images/placeholders/bg3.jpg",
-	"/images/placeholders/bg0.jpg",
-	"/images/placeholders/bg4.jpg",
-	"/images/placeholders/bg5.jpg",
-	"/images/placeholders/bg1.jpg",
-	"/images/placeholders/bg2.jpg",
+	"/images/bg/bg1.jpg",
+	"/images/bg/bg0.jpg",
+	"/images/bg/bg2.jpg",
+	"/images/bg/bg3.jpg",
+	"/images/bg/bg4.jpg",
+	"/images/bg/bg5.jpg",
+	"/images/bg/bg6.jpg",
+	"/images/bg/bg7.jpg",
 ];
 
 export const videoSources = {
